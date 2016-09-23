2a194e6718738688141af7acabb221762789bf2e
Deesen <dh@fuseit.de>
2016-09-23 20:30:05 +0200

    Experimental #688

---
+++ assets/snippets/mgrtpl/snippet.mgrtpl.inc.php
@@ -0,0 +1,36 @@
+<?php
+$get = isset($get) ? $get : '';
+$outerTpl = isset($outerTpl) ? $outerTpl : '';
+$rowTpl = isset($rowTpl) ? $rowTpl : '';
+$useTabs = isset($useTabs) ? $useTabs : '';
+$useSections = isset($useSections) ? $useSections : '';
+$filter = isset($filter) ? $filter : '';
+$sortBy = isset($sortBy) ? $sortBy : '';
+
+$tpe =& $modx->manager->tpl;
+
+switch($get) {
+	case 'css':
+		return $tpe->mergeDomCss();
+		break;
+	case 'javascript':
+		return $tpe->mergeDomJs();
+		break;
+	case 'buttons':
+		return $tpe->mergeDomActionButtons($outerTpl, $rowTpl);
+		break;
+	case 'alerts':
+		return $tpe->mergeDomAlerts($outerTpl, $rowTpl);
+		break;
+	case 'body':
+		return $tpe->mergeDomBody();
+		break;
+	case 'inputs':
+		return $tpe->mergeFormInputs($form, $outerTpl, $rowTpl, $useTabs, $useSections, $filter, $sortBy);
+		break;
+	case 'footer':
+		// return $tpe->mergeDomActionButtons($outerTpl, $rowTpl);
+		break;
+}
+return '';
+?>
