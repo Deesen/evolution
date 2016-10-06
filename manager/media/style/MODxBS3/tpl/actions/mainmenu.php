c58af00c1004c4cb2e356c0ebf4fcead96226dd6
Deesen <dh@fuseit.de>
2016-10-06 14:25:13 +0200

    MODxBS3 mainmenu snapshot (not yet working) #764

---
+++ manager/media/style/MODxBS3/tpl/actions/mainmenu.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<div id="tocText" [+tocTextRTL+]></div>
+<div id="topbar">
+	<div id="topbar-container">
+
+		<div id="statusbar">
+			<span id="buildText"></span>
+			<span id="workText"></span>
+		</div>
+
+		<div id="supplementalNav">
+			<span class="username">[+username+]</span>' [+change_password+]
+			
+			<a href="index.php?a=8" target="_top">[+lang.logout+]</a>
+			[+systemversion+]
+		</div>
+
+	</div>
+</div>
+
+<form name="menuForm" action="l4mnu.php" class="clear">
+	<input type="hidden" name="sessToken" id="sessTokenInput" value="[+sessTokenInput+]" />
+	[[mgrTpl?
+		&get=`elements`
+		&element=`mainmenu`
+	]]
+</form>
